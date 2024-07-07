1dc4f40569a482601e120f71611f0d21ae2edec7
matheusdevelloper <evangeli985107620@gmail.com>
2024-07-06 23:00:53 -0300

    Projeto Order de pedido 99% contruído com sucesso!

--- src/application/App.java
+++ src/application/App.java
@@ -1,5 +1,57 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities_enums.OrderStatus;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        
+
+        System.out.println("Enter cliente data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Birth data (DD/MM/YYYY): ");
+        String birthStr = sc.nextLine();
+        LocalDate birth = LocalDate.parse(birthStr, fmt);
+        
+
+        System.out.println("Enter order data: ");
+        System.out.print("Status: ");
+        String status = sc.nextLine();
+        Order order = new Order(LocalDateTime.now(), OrderStatus.valueOf(status), new Client(name, email, birth));
+        
+
+        System.out.print("How many items to this order? ");
+        int items = sc.nextInt();
+        sc.nextLine();
+
+        for(int i=0; i<items; i++){
+
+            System.out.println("Enter #"+(i+1)+" item data:");
+            System.out.print("Product name: ");
+            String nameProduct = sc.nextLine();
+            System.out.print("Product price: ");
+            double price = sc.nextDouble();
+            System.out.print("Quantity: ");
+            int quantity = sc.nextInt();
+            sc.nextLine();
+            OrderItem orderItem = new OrderItem(quantity, price, new Product(nameProduct, price));
+            order.addItem(orderItem);
+    
+        }
+        System.out.println();
+        System.out.println(order.toString());
+
     }
 }
--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,51 +1,75 @@
 package entities;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 import entities_enums.OrderStatus;
 
 public class Order {
-
+    
+    DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private LocalDateTime moment;
     private OrderStatus status;
-
+    
+    private Client client;
     private List<OrderItem> items = new ArrayList<>();
 
     public Order() {
     }
 
-    public Order(LocalDateTime moment, OrderStatus status) {
+
+    public Order(LocalDateTime moment, OrderStatus status, Client client) {
         this.moment = moment;
         this.status = status;
+        this.client = client;
     }
+
+    public void addItem(OrderItem item){
+        items.add(item);
+    }
+
+    public void removeItem(OrderItem item) {
+        items.remove(item);
+    }
+
 
     public LocalDateTime getMoment() {
         return moment;
     }
 
+
     public void setMoment(LocalDateTime moment) {
         this.moment = moment;
     }
+
 
     public OrderStatus getStatus() {
         return status;
     }
 
+
     public void setStatus(OrderStatus status) {
         this.status = status;
     }
 
+
+    public Client getClient() {
+        return client;
+    }
+
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
     
-    public void addItem(OrderItem item){
-
-    }
-
-    public void removeItem(OrderItem item) {
-
-    }
-
     public Double total(){
         Double sum = 0.0;
         for(OrderItem item : items){
@@ -53,5 +77,24 @@ public class Order {
         }
         return sum;
     }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ORDER SUMMARY:\n");
+        sb.append("Order moment: "+fmt1.format(moment)+"\n");
+        sb.append("Order Status: "+status+"\n");
+        sb.append("Client: "+client.getName()+" ("+fmt2.format(client.getBirthDate())+") - ("+client.getEmail()+") \n");
+        sb.append("Order items:\n");
+        for(OrderItem item : items){
+           sb.append(item.getProduct().getName()+", $"+String.format("%.2f", item.getProduct().getPrice())
+                 +", Quantity: "+item.getQuantity()+", SubTotal: $"+String.format("%.2f", item.subTotal())+"\n");
+        }
+        sb.append("Total price: $"+String.format("%.2f", total()));
+        return sb.toString();
+        
+    }
+    
     
 }
